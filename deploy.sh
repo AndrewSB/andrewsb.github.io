fea9bc2ab77ff197c16a957c1e4f2597451521b7
Andrew Breckenridge <asbreckenridge@me.com>
2016-04-14 10:49:49 -0700

    parallelize deploy script + add other domains

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
 #/!bin/bash
 
 motion build
-surge .motion/build http://andrewsb.me
+
+function joinStrings { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+
+domains=( "http://andrewsb.me" "http://ndrew.me" "http://swagso.sexy" )
+# domainStrings= joinStrings ', ' ${domains[*]}
+
+for domain in "${domains[@]}"; do
+	surge .motion/build $domain &
+done
+
+echo deploying 🙂 
+
+wait
+echo 🤑
