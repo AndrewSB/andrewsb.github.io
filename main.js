9c1b0bc7a3f79873cef1d3fde8b35632e0115a4b
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-04 00:47:04 -0700

    move the view code from main => home

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
 view Main {
-  var displayStyle = 'none';
-  setTimeout(() => displayStyle = 'flex', 1.5 * 1000)
-
-  <Hello />
-  <SocialIcons display={displayStyle} />
-  <Bottom display={displayStyle} />
-
-  $ = {
-    height: '100vh',
-    width: '100vw',
-    color: 'white',
-    background: '#66308a',
-    padding: 20,
-    position: 'absolute',
-    overflow: 'hidden',
-  }
+  <Home />
 }
